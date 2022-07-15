eaa4a72cfc643e35a69d568b2173dedded99d117
Вадим Сериков <vadimserikov@MacBook-Air-Vadim.local>
2022-07-15 16:23:18 +0300

    Решил задачи

---
+++ задача41/Program.cs
@@ -0,0 +1,31 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+//0, 7, 8, -2, -2 -> 2
+
+//1, -7, 567, 89, 223-> 3
+Console.WriteLine("Введите множество чисел, через Enter");
+int M = int.Parse(Console.ReadLine());
+int[] Numbers = new int[M];
+
+void YourNumbers(int M)
+{
+    for (int i = 0; i < M; i++)
+    {
+        Console.WriteLine($" {i}");
+        Numbers[i] = int.Parse(Console.ReadLine());
+    }
+}
+int Positive(int[] Numbers)
+{
+    int count = 0;
+    for (int i = 0; i < Numbers.Length; i++)
+    {
+        if (Numbers[i] > 0) count++;
+    }
+    return count;
+}
+
+YourNumbers(M);
+
+Console.WriteLine($"Введено чисел больше 0: {Positive(Numbers)} ");
+
